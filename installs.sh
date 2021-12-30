92e511fcdc8da566b6451f16934c7ee28563364f
etchichury <joaoetchichury@hotmail.com>
2021-12-30 18:33:06 -0300

    Add install automation

---
+++ installs.sh
@@ -0,0 +1,22 @@
+sudo apt install -y dconf-editor
+sudo apt install -y xclip
+sudo apt install -y fonts-firacode
+sudo apt install -y snapd
+sudo apt install -y zsh
+chsh -s $(which zsh)
+
+sudo snap install --classic code
+
+sudo snap install chromium
+sudo snap install htop
+sudo snap install spotify
+sudo snap install postman
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
