73e93459abc0d1b1ea83612275274ac02250fae1
etchichury <joaoetchichury@hotmail.com>
2022-04-25 17:22:43 -0300

    Tranform gitconfig file into bash script

---
+++ gitconfig.sh
@@ -0,0 +1,6 @@
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.oneline 'log --oneline'
+git config --global alias.discard 'checkout --'
+git config --global alias.last 'log -1 HEAD'
