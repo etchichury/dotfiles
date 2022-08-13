8edb79c2ddaa9adcc8d240de436515a08bf153c6
etchichury <joaoetchichury@hotmail.com>
2022-08-13 02:31:10 -0300

    Add amend alias

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,7 +1,8 @@
 git config --global alias.co checkout
 git config --global alias.ci commit
+git config --global alias.amend 'commit --amend'
 git config --global alias.unstage 'reset HEAD --'
+git config --global alias.discard 'reset --hard'
 git config --global alias.oneline 'log --oneline'
-git config --global alias.discard 'reset HEAD --hard'
 git config --global alias.last 'log -1 HEAD'
 
