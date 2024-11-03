ef19d0c730bbd3332b0ba2726fff41deedc430b7
João Soares <joaoetchichury@hotmail.com>
2024-11-02 22:36:19 -0300

    Set Neovim as default git editor

--- gitconfig.sh
+++ gitconfig.sh
@@ -7,3 +7,4 @@ git config --global alias.discard 'reset --hard'
 git config --global alias.oneline 'log --oneline'
 git config --global alias.last 'log -1 HEAD'
 git config --global pager.branch false
+git config --global core.editor nvim
