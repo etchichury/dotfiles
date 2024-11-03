e6ff716d3bd383e33b466b0a80fe2b313725b048
João Soares <joaoetchichury@hotmail.com>
2024-11-02 22:32:55 -0300

    Create git status alias

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,5 +1,6 @@
 git config --global alias.co checkout
 git config --global alias.ci commit
+git config --global alias.st status
 git config --global alias.amend 'commit --amend'
 git config --global alias.unstage 'reset HEAD --'
 git config --global alias.discard 'reset --hard'
